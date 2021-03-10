fa5549769c7e1d8d158722235001590edc8d7cba
Сезер Мехмед <63499161+sezermehmed0@users.noreply.github.com>
2021-03-10 20:37:39 +0200

    Update Elf.java

--- src/Figures/Elf.java
+++ src/Figures/Elf.java
@@ -1,4 +1,14 @@
 package Figures;
 
-public class Elf {
+import java.awt.*;
+
+public class Elf extends Figure {
+    public Elf(int row, int col, Color color) {
+        super(row, col);
+        this.attackDamage = 8;
+        this.armor = 1;
+        this.healthPoints = 10;
+        this.attackingSquares = 3;
+        this.movementSpeed = 3;
+    }
 }
