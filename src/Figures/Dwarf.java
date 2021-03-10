9c4a3aabee543615e0d37fcd93a59bd6e2edde1e
Сезер Мехмед <63499161+sezermehmed0@users.noreply.github.com>
2021-03-10 20:36:54 +0200

    Update Dwarf.java

--- src/Figures/Dwarf.java
+++ src/Figures/Dwarf.java
@@ -1,4 +1,15 @@
 package Figures;
 
-public class Dwarf {
+
+import java.awt.*;
+
+public class Dwarf extends Figure {
+    public Dwarf(int row, int col,Color color) {
+        super(row, col);
+        this.attackDamage = 6;
+        this.armor = 2;
+        this.healthPoints = 12;
+        this.attackingSquares = 2;
+        this.movementSpeed = 2;
+    }
 }
