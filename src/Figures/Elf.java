16e31c6636ebabd305973db9eec26bee844c97e1
Сезер Мехмед <63499161+sezermehmed0@users.noreply.github.com>
2021-03-11 21:02:44 +0200

    Update Elf.java

--- src/Figures/Elf.java
+++ src/Figures/Elf.java
@@ -1,5 +1,6 @@
 package Figures;
 
+import Tile.Tile;
 import java.awt.*;
 
 public class Elf extends Figure {
@@ -10,5 +11,20 @@ public class Elf extends Figure {
         this.healthPoints = 10;
         this.attackingSquares = 3;
         this.movementSpeed = 3;
+
+
     }
-}
+
+    @Override
+    public void paint(Graphics g) {
+        int x = this.col * Tile.TILE_SIZE;
+        int y = this.row * Tile.TILE_SIZE;
+
+        g.setColor(this.color);
+        g.fillRect(x + 10,y + 10,50,50);
+        g.setColor(Color.BLACK);
+        g.drawString(" E ", x + 30, y + 40);
+    }
+
+    }
+
