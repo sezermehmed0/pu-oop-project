bea85d3cf0daf80123ef6655d05581ded8c16df0
Сезер Мехмед <63499161+sezermehmed0@users.noreply.github.com>
2021-03-12 19:14:47 +0200

    Update Tile.java

--- src/Tile/Tile.java
+++ src/Tile/Tile.java
@@ -1,4 +1,26 @@
 package Tile;
+import java.awt.*;
 
-public class Tile {
+public abstract class Tile {
+
+    protected int row;
+    protected int col;
+    public static final int TILE_SIZE = 90;
+ 
+
+    /**
+     * 
+     * @param row row
+     * @param col column
+     */
+     
+     
+    public Tile (int row, int col) {
+
+        this.row = row;
+        this.col = col;
+    }
+          
+   public abstract void renderTile(Graphics g);
+ 
 }
