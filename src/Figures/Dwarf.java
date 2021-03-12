941d3b07092f32cb9c1ad053a93817095e3950d4
Сезер Мехмед <63499161+sezermehmed0@users.noreply.github.com>
2021-03-12 19:19:52 +0200

    Update Dwarf.java

--- src/Figures/Dwarf.java
+++ src/Figures/Dwarf.java
@@ -3,24 +3,31 @@ package Figures;
 import Tile.Tile;
 import java.awt.*;
 
-public class Dwarf extends Figure {
-    public Dwarf(int row, int col,Color color) {
+
+
+public class Dwarf extends Figure{
+
+ 
+
+	public Dwarf(int row, int col, Color color) {
         super(row, col);
+        this.color = color;
         this.attackDamage = 6;
         this.armor = 2;
-        this.healthPoints = 12;
-        this.attackingSquares = 2;
-        this.movementSpeed = 2;
+        this.healtPoints = 12;
+        this.possibleSquresToAttack = 2;
+        this.speed = 2;
+       
     }
 
     @Override
-    public void paint(Graphics g){
-    int x = this.col * Tile.TILE_SIZE;
-    int y = this.row * Tile.TILE_SIZE;
+    public void paint(Graphics g) {
+        int x = this.col * Tile.TILE_SIZE;
+        int y = this.row * Tile.TILE_SIZE;
 
         g.setColor(this.color);
-        g.fillRect(x + 10,y + 10,50,50);
+        g.fillRect(x + 10,y + 20,60,60);
         g.setColor(Color.BLACK);
-        g.drawString(" Dw ", x + 30, y + 40);
-}
+        g.drawString(" Dw ", x + 25, y + 50);
+    }
 }
