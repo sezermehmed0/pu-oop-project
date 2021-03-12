33523ccb5a62e0960540891b4919132bba1ea0a4
Сезер Мехмед <63499161+sezermehmed0@users.noreply.github.com>
2021-03-12 19:18:38 +0200

    Update Elf.java

--- src/Figures/Elf.java
+++ src/Figures/Elf.java
@@ -1,18 +1,21 @@
 package Figures;
 
 import Tile.Tile;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class Elf extends Figure {
+
+public class Elf extends Figure{
+
     public Elf(int row, int col, Color color) {
         super(row, col);
-        this.attackDamage = 8;
+        this.color = color;
+        this.attackDamage = 5;
         this.armor = 1;
-        this.healthPoints = 10;
-        this.attackingSquares = 3;
-        this.movementSpeed = 3;
-
-
+        this.healtPoints = 10;
+        this.possibleSquresToAttack = 3;
+        this.speed = 3;
+      
     }
 
     @Override
@@ -21,10 +24,11 @@ public class Elf extends Figure {
         int y = this.row * Tile.TILE_SIZE;
 
         g.setColor(this.color);
-        g.fillRect(x + 10,y + 10,50,50);
+        g.fillRect(x + 10,y + 20,60,60);
         g.setColor(Color.BLACK);
-        g.drawString(" E ", x + 30, y + 40);
+        g.drawString(" E ", x + 30, y + 50);
     }
 
-    }
+		
+	}
 
