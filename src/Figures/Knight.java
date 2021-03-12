8df25a6efe734c03524f2df60a6c6b7c0e335e72
Сезер Мехмед <63499161+sezermehmed0@users.noreply.github.com>
2021-03-12 19:17:19 +0200

    Update Knight.java

--- src/Figures/Knight.java
+++ src/Figures/Knight.java
@@ -1,31 +1,31 @@
 package Figures;
 
 
-import Tile.Tile;
-
 import java.awt.*;
 
-public class Knight extends Figure {
-    public Knight(int row, int col,Color color) {
+import Tile.Tile;
+
+public class Knight extends Figure{
+
+    public Knight(int row, int col, Color color) {
         super(row, col);
+        this.color = color;
         this.attackDamage = 8;
-        this.armor = 3 ;
-        this.healthPoints = 15;
-        this.attackingSquares = 1;
-        this.movementSpeed = 1;
+        this.armor = 3;
+        this.healtPoints = 15;
+        this.possibleSquresToAttack = 1;
+        this.speed = 1;
+    
     }
 
     @Override
     public void paint(Graphics g) {
-
         int x = this.col * Tile.TILE_SIZE;
         int y = this.row * Tile.TILE_SIZE;
 
         g.setColor(this.color);
-        g.fillRect(x + 10,y + 10,50,50);
+        g.fillRect(x + 10,y + 10,60,60);
         g.setColor(Color.BLACK);
-        g.drawString(" Kn ", x + 30, y + 40);
+        g.drawString(" Kn ", x + 25, y + 40);
     }
-
-    }
-
+}
